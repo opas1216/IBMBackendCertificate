d78b13e171d61a5669ad4b6c486818da1533319a
Michael Hsieh <opas1216@hotmail.com>
2023-03-21 01:15:37 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+class AnalysedText(object):
+
+
+
+
+    def __init__(self, text):
+
+        pass
+
+
+
+text2 = "Michael is handsome MichaelMichaelMichaelMichael"
+text3 = text2.replace('Michael', 'Julie')
+print(f"Text3 {text3}")
+print(f"Type of {text2.split()} is {type(text2.split())}")
+print(f"Count of Michael = {text2.count('Michael')}")
